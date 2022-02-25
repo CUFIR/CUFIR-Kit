abdedb7842e01307d69453879edde2d48d2de862
China Universal Financial Industry message Repository <97862260+CUFIR@users.noreply.github.com>
2022-02-25 18:36:05 +0800

    remove code and upload
    remove old code and upload new old

--- cufir-s-data/src/main/java/com/cfets/cufir/s/data/util/ConstantUtil.java
+++
@@ -1,9 +0,0 @@
-package com.cfets.cufir.s.data.util;
-
-public class ConstantUtil {
-
-	public static String PROVISIONALLY_REGISTERED = "Provisionally Registered";
-	public static String OBSOLETE = "Obsolete";
-	public static String REGISTERED = "Registered";
-	public static String ADDED_REGISTERED = "Added Registered";
-}
